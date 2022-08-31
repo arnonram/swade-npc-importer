f083cb8096d950ef95de361412a4d1163e3f6306
Kristian Serrano <kristianserrano@gmail.com>
2022-08-31 18:07:02 -0400

    Minor corrections

--- scripts/swadeNpcImporter.js
+++ scripts/swadeNpcImporter.js
@@ -29,11 +29,15 @@ Hooks.on('ready', async () => {
 
 Hooks.on('renderActorDirectory', async (app, html, data) => {
   if (game.users.get(game.userId).can('ACTOR_CREATE') == true) {
-    const npcImporterButton = `<button style="width: calc(100% - 8px);"><i class="fas fa-align-left"></i>${game.i18n.localize('npcImporter.HTML.StatBlockImporterTitle')}</button>`;
+    const npcImporterButton = $(
+      `<button style="width: calc(100% - 8px);"><i class="fas fa-align-left"></i>${game.i18n.localize(
+        'npcImporter.HTML.StatBlockImporterTitle'
+      )}</button>`
+    );
 
     html.find('.directory-footer').append(npcImporterButton);
 
-    npcImporterButton.click(() => {
+    npcImporterButton.on('click', () => {
       new Dialog({
         title: game.i18n.localize('npcImporter.HTML.ImportTitle'),
         content: importerDialogue(),
