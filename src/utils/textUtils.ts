d542b5dfbdce4f788bb66dc3a451bf1d55c597f0
Arnon Ram <arnonram@gmail.com>
2025-06-19 07:15:34 +0200

    update textUtils

--- src/utils/textUtils.ts
+++ src/utils/textUtils.ts
@@ -1,48 +1,41 @@
 import { newLineRegex } from '../global';
 
-export function capitalize(string: string): string {
-  return string.replace(/(?:^|\s)\S/g, function (a) {
-    return a.toUpperCase();
-  });
+export function capitalize(input: string): string {
+  return input.replace(/(?:^|\s)\S/g, a => a.toUpperCase());
 }
 
-export function capitalizeEveryWord(string: string): string {
-  let capitalizedString: string[] = [];
-  string.split(' ').forEach(x => {
-    capitalizedString.push(capitalize(x.toLowerCase()));
-  });
-
-  return capitalizedString
+export function capitalizeEveryWord(input: string): string {
+  return input
+    .split(' ')
+    .map(word => capitalize(word.toLowerCase()))
     .join(' ')
     .replace(/[\-\()][a-z]| [a-z]/g, match => match.toUpperCase());
 }
 
-export function specialAbilitiesLink(specAbName: string): string {
-  var cleanedName = specAbName
+export function specialAbilitiesLink(specAbName: string): string | null {
+  const cleanedName = specAbName
     .split('(')[0]
-    .replace(new RegExp('[\\−\\-\\+]? [0-9]'), '')
+    .replace(/[\−\-+]? [0-9]/, '')
     .trim();
   try {
-    // @ts-ignore: game is a global from Foundry VTT
-    return game.packs
-      .get('swade-core-rules.swade-specialabilities')
-      .contents.filter((x: any) => x.name.includes(cleanedName))[0].link;
+    return (
+      game.packs
+        ?.get('swade-core-rules.swade-specialabilities')
+        ?.contents.find((x: any) => x.name.includes(cleanedName))?.link || null
+    );
   } catch (error) {
-    return '';
+    return null;
   }
 }
 
-export function removeMultipleWhitespaces(text: string): string {
-  return text.replace(new RegExp(/\s+/g), ' ');
-}
+export const removeMultipleWhitespaces = (text: string): string =>
+  text.replace(/\s+/g, ' ');
 
-export function splitAndSort(text: string): string {
-  return JSON.stringify(
-    text
-      .split(/[\s,]+/)
-      .map(x => x.toLowerCase().trim())
-      .sort(),
-  );
+export function splitAndSort(text: string): string[] {
+  return text
+    .split(/[\s,]+/)
+    .map(x => x.toLowerCase().trim())
+    .sort();
 }
 
 export function splitAndTrim(
@@ -51,8 +44,6 @@ export function splitAndTrim(
 ): string[] {
   return stringToSplit
     .split(separator)
-    .map(function (item) {
-      return item.replace(newLineRegex, ' ').trim();
-    })
-    .filter(item => item !== '');
+    .map(item => item.replace(newLineRegex, ' ').trim())
+    .filter(item => item.length > 0);
 }
